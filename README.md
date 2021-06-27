90dd88ed5f0c6fd68f046cd575a5276e28ff8e09
YM199 <mr_yang_mou@163.com>
2021-06-27 17:22:32 +0800

    移植成功

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# LVGL
-LVGL-V8
+一、clone仓库
+
+```shell
+git clone https://github.com/YM199/LVGL.git
+git submodule init  
+git submodule update
+```
+二、设置头文件
+
+在工程根目录执行以下语句
+
+```shell
+cp ./lvgl/lv_conf_template.h ./lv_conf.h
+cp ./lv_drivers/lv_drv_conf_template.h lv_drv_conf.h
+cp ./lv_demos/lv_demo_conf_template.h lv_demo_conf.h
+```
+
+
+
+将lv_conf.h、lv_drv_conf.h、lv_demo_conf.h的第10行的 `#if 0` 改为 `#if 1`
+
+将lv_conf.h的`\#define LV_COLOR_DEPTH   32`改为`\#define LV_COLOR_DEPTH   16`
+<font color=red>注意</font>：这里对应自己显示器的硬件配置。比如我的显示器是16的（RGB565）。
+
+**lv_drv_conf.h**
+
+```C
+#  define USE_FBDEV           1 #改为1 使用framebuffer为/dev/fb0
+#  define USE_EVDEV           1 #改为1 打开输入，使用/dev/input/event2
+```
+
+你的板子的输入事件可能不是event1，根据实际进行修改。可以使用cat命令来测试一下你板子的触摸屏输入对应的是哪一个event，如：
+
+![image-20210612173308112](img/image-20210612173308112.png)
+
+此时，触摸一下屏幕，则终端会有信息（乱码信息）输出，则说明当前板子的触摸输入对应的就是这个event。
+
+
+
+**lv_ex_conf.h**
+
+主函数调用哪个demo，就把哪个demo的宏定义设置为1，例如
+
+```C
+/*Printer demo, optimized for 800x480*/
+#define LV_USE_DEMO_PRINTER     1
+```
+
+
+
+
+
+
+
+
+
+
+
+目前的github版本有一些bug，比如如下图：
+
+![image-20210627163848018](img/image-20210627163848018.png)
+
+这是属于语法错误了，只需要对着错误改掉就行。
+
+
+
+
+
+遇见`error: ‘lv_font_montserrat_xx’ undeclared (first use in this function);`，只需要去lv_conf.h将对应的宏定义
+
+`#define LV_FONT_MONTSERRAT_xx  1`打开就行
+
--- main.c
+++ main.c
@@ -19,25 +19,24 @@
 
 int main(void)
 {
-    /*LittlevGL init*/
+
     lv_init();
-    /*Linux frame buffer device init*/
+
+
+    static lv_disp_draw_buf_t draw_buf;
+    static lv_color_t buf1[DISP_HOR_RES * DISP_VER_RES / 10];                        /*Declare a buffer for 1/10 screen size*/
+    lv_disp_draw_buf_init(&draw_buf, buf1, NULL, MY_DISP_HOR_RES * MY_DISP_VER_RES / 10);  /*Initialize the display buffer.*/
+
     fbdev_init();
-
-     /*初始化图像数据缓存区*/
-     static lv_disp_draw_buf_t draw_buf;
-    static lv_color_t buf1_1[DISP_HOR_RES * 100];
-    static lv_color_t buf1_2[DISP_HOR_RES * 100];
-    lv_disp_draw_buf_init(&draw_buf, buf1_1, buf1_2, DISP_HOR_RES * 100);
-
     lv_disp_drv_t disp_drv;               /*Descriptor of a display driver*/
     lv_disp_drv_init(&disp_drv);          /*Basic initialization*/
     disp_drv.flush_cb = fbdev_flush;    /*Set your driver function*/
     disp_drv.draw_buf = &draw_buf;          /*Assign the buffer to the display*/
     disp_drv.hor_res = MY_DISP_HOR_RES;   /*Set the horizontal resolution of the display*/
-    disp_drv.hor_res = MY_DISP_VER_RES;   /*Set the verizontal resolution of the display*/
+    disp_drv.ver_res = MY_DISP_VER_RES;   /*Set the verizontal resolution of the display*/
     lv_disp_drv_register(&disp_drv);      /*Finally register the driver*/
 
+    evdev_init();
     lv_indev_drv_t indev_drv;                  /*Descriptor of a input device driver*/
     lv_indev_drv_init(&indev_drv);             /*Basic initialization*/
     indev_drv.type = LV_INDEV_TYPE_POINTER;    /*Touch pad is a pointer-like device*/
@@ -45,7 +44,7 @@ int main(void)
     lv_indev_drv_register(&indev_drv);         /*Finally register the driver*/
 
 
-    lv_demo_widgets();
+    lv_example_get_started_1();
     while (1)
     {
         lv_tick_inc(5);
